db7d0d3c19dc1201442b16c39d8540cba37bfa6c
memmsc <xiongfei>
2019-04-10 17:25:12 +0800

    add mybatis framework

---
+++ battcn-framework/mybatis-framework-starter/src/main/java/com/battcn/framework/mybatis/service/BaseService.java
@@ -0,0 +1,59 @@
+package com.battcn.framework.mybatis.service;
+
+import com.battcn.framework.mybatis.entity.DataGrid;
+
+import java.util.List;
+
+/**
+ * 快速递公共 Mapper
+ *
+ * @param <T> 实体类
+ * @author Levin
+ */
+public interface BaseService<T> {
+
+
+    int deleteByPrimaryKey(Object key);
+
+    int delete(T record);
+
+    int insert(T record);
+
+    int insertSelective(T record);
+
+    boolean existsWithPrimaryKey(Object key);
+
+
+    List<T> selectAll();
+
+    T selectByPrimaryKey(Object key);
+
+
+    int selectCount(T record);
+
+
+    List<T> select(T record);
+
+
+    T selectOne(T record);
+
+
+    int updateByPrimaryKey(T record);
+
+    int updateByPrimaryKeySelective(T record);
+
+    int insertList(List<? extends T> list);
+
+    int insertUseGeneratedKeys(T record);
+
+
+    ResultDataGrid<List<T>> queryByDataGrid(DataGrid grid);
+
+    ResultDataGrid<List<T>> queryByDataGrid(DataGrid grid, T entity);
+
+    List<T> selectByExample(Object var1);
+
+    T selectOneByExample(Object var1);
+
+    int updateByExampleSelective(T record, Object example);
+}
